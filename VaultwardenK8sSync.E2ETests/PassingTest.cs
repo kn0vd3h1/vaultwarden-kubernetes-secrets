4ec8c1aa3c90e4c5089e35a85055c981f64522be
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-17 19:50:45 +0000

    Add temporary change

---
+++ VaultwardenK8sSync.E2ETests/PassingTest.cs
@@ -0,0 +1,1 @@
+using Xunit; namespace VaultwardenK8sSync.E2ETests.Tests { public class PassingTest { [Fact] public void Test1() { Assert.True(true); } } }
---
+++ VaultwardenK8sSync.Tests/SimpleTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace VaultwardenK8sSync.Tests
+{
+    public class SimpleTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
